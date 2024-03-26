0fe52fcb8360b7a8e8bfe7ae9cdf353edeaff0e2
Eduardo Squetini <163067276+eduardo-squetini@users.noreply.github.com>
2024-03-26 00:36:11 -0300

    Update Desafio_Calculadora.java

--- Desafio_Calculadora.java
+++ Desafio_Calculadora.java
@@ -1,26 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package desafiocalculadora_aula05;
 import java.util.Scanner;
-/**
- *
- * @author Eduardo
- */
+
 public class DesafioCalculadora_aula05 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        //desafio calculadora: Desenvolver uma calculadora que realize as 4 operações básicas. A execução deve ser decidida por meio de um menu.
+        // Challenge calculator: Develop a calculator that performs the 4 basic operations. The execution should be decided through a menu.
         
-        //índice das variáveis no início do programa
+        // Variable index at the beginning of the program
         int operacoes = 0;
         double calculo1 = 0;
         double calculo2 = 0;
-        
         
         Scanner leitor = new Scanner(System.in);
         System.out.println("Coloque 1 para soma");
@@ -29,47 +17,33 @@ public class DesafioCalculadora_aula05 {
         System.out.println("Coloque 4 para multiplicacao");
         operacoes = leitor.nextInt();
         
-        
-        if((operacoes == 1) || (operacoes == 2) || (operacoes == 3) || (operacoes == 4)){
-            
+        if((operacoes >= 1) && (operacoes <= 4)){
             System.out.println("Digite seu primeiro numero");
             calculo1 = leitor.nextDouble();
             System.out.println("Digite seu segundo numero");
             calculo2 = leitor.nextDouble();
             System.out.println("Sua resposta e: ");
             
-        } 
-        
-        
-        switch(operacoes){
-                
-            case 1:
-                System.out.println(calculo1 + calculo2);
-                break;
-            case 2:
-                System.out.println(calculo1 - calculo2);
-                break;
-            case 3:
-                System.out.println(calculo1 / calculo2);
-                break;
-            case 4:
-                System.out.println(calculo1 * calculo2);
-                break;
-            default:
-                System.out.println("opcao invalida");
-                
+            switch(operacoes){
+                case 1:
+                    System.out.println(calculo1 + calculo2);
+                    break;
+                case 2:
+                    System.out.println(calculo1 - calculo2);
+                    break;
+                case 3:
+                    System.out.println(calculo1 / calculo2);
+                    break;
+                case 4:
+                    System.out.println(calculo1 * calculo2);
+                    break;
+                default:
+                    System.out.println("opcao invalida");
+            }
+        } else {
+            System.out.println("Opção inválida. Por favor, escolha um número entre 1 e 4.");
         }
         
-        
-       
-            
-            
-        
-        
-        
-        
-        
-        // TODO code application logic here
+        leitor.close(); // It's a good practice to close the scanner
     }
-    
 }
