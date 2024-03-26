358c37a714ca52951d6aa2e387abf00d99b02bbe
Eduardo Squetini <163067276+eduardo-squetini@users.noreply.github.com>
2024-03-26 00:42:42 -0300

    Update Estudante.java

--- Estudante.java
+++ Estudante.java
@@ -1,60 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package excecoes_try_catch;
 
-/**
- *
- * @author Eduardo
- */
 public class Estudante {
    
-	private String nome;
-	private double a1, a2, a3, mediaFinal;
+    private String nome;
+    private double a1, a2, a3, mediaFinal;
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public double getA1() {
-		return a1;
-	}
+    public double getA1() {
+        return a1;
+    }
 
-	public void setA1(double a1) {
-		this.a1 = a1;
-	}
+    public void setA1(double a1) {
+        this.a1 = a1;
+    }
 
-	public double getA2() {
-		return a2;
-	}
+    public double getA2() {
+        return a2;
+    }
 
-	public void setA2(double a2) {
-		this.a2 = a2;
-	}
+    public void setA2(double a2) {
+        this.a2 = a2;
+    }
 
-	public double getA3() {
-		return a3;
-	}
+    public double getA3() {
+        return a3;
+    }
 
-	public void setA3(double a3) {
-		this.a3 = a3;
-	}
+    public void setA3(double a3) {
+        this.a3 = a3;
+    }
 
-	public double getMediaFinal() {
-		return mediaFinal;
-	}
+    public double getMediaFinal() {
+        return mediaFinal;
+    }
 
-	public void setMediaFinal(double mediaFinal) {
-		this.mediaFinal = mediaFinal;
-	}
+    public void setMediaFinal(double mediaFinal) {
+        this.mediaFinal = mediaFinal;
+    }
 
-	public double calcularMediaFinal() {
-		return mediaFinal = (a1 * 0.3) + (a2 * 0.3) + (a3 * 0.4);
-	}
+    public double calcularMediaFinal() {
+        return mediaFinal = (a1 * 0.3) + (a2 * 0.3) + (a3 * 0.4);
+    }
 }
-
